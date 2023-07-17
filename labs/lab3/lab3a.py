dfe82ecd643a77cb0efe34644f661b2ec2e659a6
Sre-A <96755517+Sre-A@users.noreply.github.com>
2023-07-17 14:33:04 -0400

    Add files via upload

---
+++ labs/lab3/lab3a.py
@@ -0,0 +1,108 @@
+"""
+Copyright MIT and Harvey Mudd College
+MIT License
+Summer 2020
+
+Lab 3A - Depth Camera Safety Stop
+"""
+
+########################################################################################
+# Imports
+########################################################################################
+
+import sys
+import cv2 as cv
+import numpy as np
+
+sys.path.insert(0, "../../library")
+import racecar_core
+import racecar_utils as rc_utils
+
+########################################################################################
+# Global variables
+########################################################################################
+
+rc = racecar_core.create_racecar()
+
+# Add any global variables here
+
+########################################################################################
+# Functions
+########################################################################################
+
+
+def start():
+    """
+    This function is run once every time the start button is pressed
+    """
+    # Have the car begin at a stop
+    rc.drive.stop()
+
+    # Print start message
+    print(
+        ">> Lab 3A - Depth Camera Safety Stop\n"
+        "\n"
+        "Controls:\n"
+        "    Right trigger = accelxerate forward\n"
+        "    Right bumper = override safety stop\n"
+        "    Left trigger = accelerate backward\n"
+        "    Left joystick = turn front wheels\n"
+        "    A button = print current speed and angle\n"
+        "    B button = print the distance at the center of the depth image"
+    )
+
+
+def update():
+    """
+    After start() is run, this function is run every frame until the back button
+    is pressed
+    """
+    # Use the triggers to control the car's speed
+    rt = rc.controller.get_trigger(rc.controller.Trigger.RIGHT)
+    lt = rc.controller.get_trigger(rc.controller.Trigger.LEFT)
+    speed = rt - lt
+
+    # Calculate the distance of the object directly in front of the car
+    depth_image = rc.camera.get_depth_image()
+    center_distance = rc_utils.get_depth_image_center_distance(depth_image)
+
+    # Define the desired distance from the cone (in meters)
+    desired_distance = 0.127  # Approximately 5 inches in meters
+
+    # Calculate the difference between the desired distance and the actual distance
+    distance_error = center_distance - desired_distance
+
+    # Define a proportional control gain
+    proportional_gain = 1.0
+
+    # Adjust the speed based on the distance error
+    speed -= proportional_gain * distance_error
+
+    # Clamp the speed between -1.0 and 1.0
+    speed = max(min(speed, 1.0), -1.0)
+
+    # Use the left joystick to control the angle of the front wheels
+    angle = rc.controller.get_joystick(rc.controller.Joystick.LEFT)[0]
+
+    rc.drive.set_speed_angle(speed, angle)
+
+    # Print the current speed and angle when the A button is held down
+    if rc.controller.is_down(rc.controller.Button.A):
+        print("Speed:", speed, "Angle:", angle)
+
+    # Print the depth image center distance when the B button is held down
+    if rc.controller.is_down(rc.controller.Button.B):
+        print("Center distance:", center_distance)
+
+    # Display the current depth image
+    rc.display.show_depth_image(depth_image)
+
+
+
+########################################################################################
+# DO NOT MODIFY: Register start and update and begin execution
+########################################################################################
+
+if __name__ == "__main__":
+    rc.set_start_update(start, update, None)
+    rc.go()
